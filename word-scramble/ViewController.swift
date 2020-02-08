536dadafa096011e4ba3e23b91b5ef2bb5270212
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-02-08 10:19:53 +0000

    Initial project setup

---
+++ word-scramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  word-scramble
+//
+//  Created by Bradley Chesworth on 08/02/2020.
+//  Copyright © 2020 Brad Chesworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
