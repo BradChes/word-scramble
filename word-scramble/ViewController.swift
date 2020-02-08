8bb2102b1abc86e0b8d9732820c1791a845c0d03
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-02-08 11:13:53 +0000

    Reading words from a static file
    Also added foundations for tableview controller to show used words.

--- word-scramble/ViewController.swift
+++ word-scramble/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    private var allWords = [String]()
+    private var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let startWordsUrl = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsUrl) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
     }
-
-
+    
+    private func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
 }
 
